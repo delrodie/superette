4f72cc4f23a0a495a2c0938257dccfb5f94ee7d9
Dieudonne AMOIKON <delrodieamoikon@gmail.com>
2019-12-19 05:24:24 +0000

    Etats inventaire liste

---
+++ src/AppBundle/Controller/EtatInventaireController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Inventaire;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+/**
+ * Class EtatInventaireController
+ * @Route("/etat/inventaire")
+ */
+class EtatInventaireController extends Controller
+{
+    /**
+     * @Route("/", name="etat_inventaire_liste")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $inventaires = $em->getRepository("AppBundle:Inventaire")->findAll();
+
+        return $this->render("etat/inventaire_list.html.twig",[
+            'inventaires' => $inventaires
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="etat_inventaire_show")
+     * @Method("GET")
+     */
+    public function showAction(Inventaire $inventaire)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $inventoriers = $em->getRepository("AppBundle:Inventorier")->findBy(['inventaire'=>$inventaire->getId()]);
+
+        return $this->render("etat/inventaire_details.html.twig",[
+            'inventoriers' => $inventoriers,
+            'inventaire' => $inventaire
+        ]);
+    }
+}
