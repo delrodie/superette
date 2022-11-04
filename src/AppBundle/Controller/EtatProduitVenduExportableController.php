adaa532f3b12311bb8e84eb28f712fd86b8cd058
Dieudonne AMOIKON <delrodie@users.noreply.github.com>
2022-11-04 13:48:01 +0000

    Update EtatProduitVenduExportableController.php

--- src/AppBundle/Controller/EtatProduitVenduExportableController.php
+++ src/AppBundle/Controller/EtatProduitVenduExportableController.php
@@ -6,8 +6,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 /**
+ * @Security("has_role('ROLE_ADMIN')")
  * @Route("/produit-vendu-exportable")
  */
 class EtatProduitVenduExportableController extends Controller
